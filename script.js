6e1088087c5aec27ed4e95191d1a630737489e0b
Naira_Reyes <gab1ana1@gmail.com>
2021-03-03 22:49:13 -0500

    Added more details such as max-min temp, wind and visibility.  Fixed sunrise-sunset time format

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const temperature = document.querySelector('.temp')
 const sunriseInfo = document.querySelector('.card-text3')
 const sunsetInfo = document.querySelector('.card-text4')
 const locationName = document.querySelector('.location')
+const feelsLikeTemp = document.querySelector('.feels-like')
+const maxTempInfo = document.querySelector('.max-temp')
+const minTempInfo = document.querySelector('.min-temp')
+const windInfo = document.querySelector('.wind')
+const visibilityInfo = document.querySelector('.visibility')
 
 
 
@@ -22,14 +27,21 @@ const getWeatherInfo = () => {
             const tempMax = data.main.temp_max
             const sunrise = data.sys.sunrise
             const sunset = data.sys.sunset
+            const wind = data.wind.speed
+            const visibility = data.visibility
+
 
             weatherIcon.src = `http://openweathermap.org/img/w/${icon}.png`;
             currentWeather.innerHTML = `${allCaps(weatherDescription)}`;
             temperature.innerHTML = `${convertTemp(temp)}°F`;
-            sunriseInfo.innerHTML = sunrise
-            sunsetInfo.innerHTML = sunset
+            sunriseInfo.innerHTML = `${formatTime(sunrise)}`
+            sunsetInfo.innerHTML = `${formatTime(sunset)}`
             locationName.innerHTML = `${allCaps(nameValue)}`;
-
+            feelsLikeTemp.innerHTML = `${convertTemp(feelsLike)}°F`
+            maxTempInfo.innerHTML = `${convertTemp(tempMax)}°F`
+            minTempInfo.innerHTML = `${convertTemp(tempMin)}°F`
+            windInfo.innerHTML = `${convertSpeed(wind)} mph`
+            visibilityInfo.innerHTML = `${convertDistance(visibility)} mi`
 
 
             console.log(nameValue, weatherDescription)
@@ -42,3 +54,23 @@ const convertTemp = temp => {
 }
 
 const allCaps = str => str.toUpperCase()
+
+const convertSpeed = speed => {
+    const mph = speed * 2.237
+    return mph.toFixed(2)
+}
+
+const convertDistance = distance => {
+    const miles = distance / 1609
+    return miles.toFixed(2)
+}
+
+const formatTime = time => {
+    const date = new Date(time * 1000);
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const seconds = "0" + date.getSeconds();
+    // Display time in 10:30:23 format
+    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+}
